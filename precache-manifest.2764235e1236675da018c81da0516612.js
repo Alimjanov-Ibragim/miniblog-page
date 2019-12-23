da9c93984a3364c8ac8b65ecd62b8c0c3ee23f64
Ibragim Alimjanov <ballismlife@gmail.com>
2019-12-23 07:18:55 +0500

    Updates

---
+++ precache-manifest.2764235e1236675da018c81da0516612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1739de52f58c10fbdeb2b92343680440",
+    "url": "/miniblog-page/index.html"
+  },
+  {
+    "revision": "684c8a631cf7333fc89c",
+    "url": "/miniblog-page/static/css/main.9ff811d3.chunk.css"
+  },
+  {
+    "revision": "b350816509a918386662",
+    "url": "/miniblog-page/static/js/2.1469ce88.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/miniblog-page/static/js/2.1469ce88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "684c8a631cf7333fc89c",
+    "url": "/miniblog-page/static/js/main.d6e16855.chunk.js"
+  },
+  {
+    "revision": "e99af27afda7f81e391a",
+    "url": "/miniblog-page/static/js/runtime-main.248fefff.js"
+  }
+]);
